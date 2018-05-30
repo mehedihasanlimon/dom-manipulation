c870fefff873778c78a94a2e4c805a9275bf585a
mehedi hasan limon <mhlimon860@gmail.com>
2018-05-30 04:59:03 +0000

    classwork4 2c

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var heading = document.getElementById('heading')
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
+//task 2A
 var textBtn =document.getElementById('textBtn2')
 textBtn.addEventListener('click',function(){
     
@@ -12,5 +13,19 @@ textBtn.addEventListener('click',function(){
     task2.appendChild(paragraph)
 })
 
+//task 2b
+function changeBGcolor(event){
+    var body =document.getElementsByTagName('body')[0]
+    if(event.target.innerText=="Red"){
+        body.style.backgroundColor= 'red';
+    }
+    if(event.target.innerText=="Green"){
+        body.style.backgroundColor= 'green';
+    }
+}
+//task 2c
 
+function redirectToGoogle(){
+    window.location.assign('http://google.com')
+}
 /* Task 4 using jQuery */
