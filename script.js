d0a95604685619de28a504644bdae08aad13f660
mehedi hasan limon <mhlimon860@gmail.com>
2018-05-30 04:19:51 +0000

    classwork4 2A

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var heading = document.getElementById('heading')
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
-
+var textBtn =document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+    
+    var task2 = document.getElementById('task2a')
+    var paragraph =document.createElement('p')
+    paragraph.innerText ="hello world"
+    task2.appendChild(paragraph)
+})
 
 
 /* Task 4 using jQuery */
